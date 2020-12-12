b83fe3426ac04b1adda34309efd1347c87a8be63
randall.erasmus <randallerasmus1@gmail.com>
2020-12-12 15:40:39 +0200

    Updating the router

--- angular-sofaworld/src/app/app-routing.module.ts
+++ angular-sofaworld/src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {AccesoriesComponent} from "./accesories/accesories.component";
+import {LookbookComponent} from "./lookbook/lookbook.component";
+import {BlogComponent} from "./blog/blog.component";
+import {PagesComponent} from "./pages/pages.component";
+import {AboutUsComponent} from "./about-us/about-us.component";
+import {ContactUsComponent} from "./contact-us/contact-us.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '',   redirectTo: 'home', pathMatch: 'full' }, // redirect to `first-component`
-  // { path: 'second-component', component: SecondComponent },
-  // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+  { path: 'accesories', component: AccesoriesComponent },
+  { path: 'lookbook', component: LookbookComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'pages', component: PagesComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'contact-us', component: ContactUsComponent },
 ];
 
 
--- angular-sofaworld/src/app/app.module.ts
+++ angular-sofaworld/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AccesoriesComponent } from './accesories/accesories.component';
+import { LookbookComponent } from './lookbook/lookbook.component';
+import { BlogComponent } from './blog/blog.component';
+import { PagesComponent } from './pages/pages.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AccesoriesComponent,
+    LookbookComponent,
+    BlogComponent,
+    PagesComponent,
+    AboutUsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
