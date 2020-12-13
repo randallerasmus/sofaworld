9fe5508fd944ed2168d49991e7ae30e83b747601
randall.erasmus <randallerasmus1@gmail.com>
2020-12-13 23:37:41 +0200

    adding the accesories component

---
+++ angular-sofaworld/src/app/accesories/accesories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accesories',
+  templateUrl: './accesories.component.html',
+  styleUrls: ['./accesories.component.css']
+})
+export class AccesoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
