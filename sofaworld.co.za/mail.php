ee31a052904391b792604190599e87d8e5002ecb
randall.erasmus <randallerasmus1@gmail.com>
2020-12-27 23:54:26 +0200

    email updates

--- sofaworld.co.za/mail.php
+++ sofaworld.co.za/mail.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n subject: $subject \n Message: $message";
-$recipient = "gabriel@strandinteriors.co.za";
+$recipient = "randallerasmus1@gmail.com";
 $subject = "Add Your Subject Here";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
